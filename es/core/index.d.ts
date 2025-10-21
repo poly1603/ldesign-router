51fdfebc8ba0d07d8f5a77e8fbbae5c27b37c212
swimly <979741120@qq.com>
2025-10-21 18:35:53 +0800

    chore: 统一配置 eslint 和 gitignore，清理构建产物

--- es/core/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @ldesign/router 核心模块导出
- */
-export * from './constants';
-export { createMemoryHistory, createWebHashHistory, createWebHistory, } from './history';
-export { RouteMatcher } from './matcher';
-export { createRouter } from './router';
-export type { RouterImpl } from './router';
--- es/index-lib.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @ldesign/router - UMD Library Entry
- *
- * This file serves as the UMD build entry point for the router library.
- * It exports all public APIs for browser usage via script tags.
- */
-import * as RouterLib from './index';
-export * from './index';
-export default RouterLib;
